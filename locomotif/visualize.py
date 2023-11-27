f061017aadc7f1bd9cdd3622ff9c2a7584ec5159
Daan Van Wesenbeeck <daanv@pinac25.cs.kuleuven.be>
2023-11-27 17:17:43 +0100

    update: ecg example with example from the paper

--- locomotif/visualize.py
+++ locomotif/visualize.py
@@ -3,17 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from cycler import cycler
 
-SMALL_SIZE = 8
-MEDIUM_SIZE = 10
-BIGGER_SIZE = 12
-
-plt.rc('font',        size=SMALL_SIZE)          
-plt.rc('axes',   titlesize=SMALL_SIZE)
-plt.rc('axes',   labelsize=MEDIUM_SIZE)
-plt.rc('xtick',  labelsize=SMALL_SIZE)
-plt.rc('ytick',  labelsize=SMALL_SIZE)
-plt.rc('legend',  fontsize=SMALL_SIZE)
-plt.rc('figure', titlesize=BIGGER_SIZE)
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIG_SIZE = 14
 
 
 def plot_motif_sets(series, motif_sets, gt=None):
@@ -27,46 +19,33 @@ def plot_motif_sets(series, motif_sets, gt=None):
         axs = [axs]
 
     axs[0].set_prop_cycle(cycler(color=[u'#00407a', u'#2ca02c', u'#c00000']))
-    axs[0].plot(range(len(series)), series, lw=1)
-    axs[0].legend([f"dim {d}" for d in range(series.shape[1])])
+    axs[0].plot(range(len(series)), series, lw=1.5)
+    axs[0].legend([f"dim {d+1}" for d in range(series.shape[1])], fontsize=SMALL_SIZE)
     axs[0].set_xlim((0, n))
 
     if gt is not None:
         plot_ground_truth_ax(axs[0], gt, n)
 
-    for i, occs in enumerate(motif_sets):
-        axs[i+1].set_title(f"Motif Set {i}, k: {len(occs)}")
-        for s_occ, e_occ in occs:
+    for i, motif_set in enumerate(motif_sets):
+        axs[i+1].set_title(f"Motif Set {i+1}, k: {len(motif_set)}", fontsize=BIG_SIZE)
+        for s_m, e_m in motif_set:
             axs[i+1].set_prop_cycle(cycler(color=[u'#00407a', u'#2ca02c', u'#c00000']))
-            axs[i+1].plot(range(s_occ, e_occ), series[s_occ : e_occ, :], alpha=1, lw=1)
-            axs[i+1].axvline(x=s_occ, c='k', linestyle=':', lw=0.25)
-            axs[i+1].axvline(x=e_occ, c='k', linestyle=':', lw=0.25)
+            axs[i+1].plot(range(s_m, e_m), series[s_m : e_m, :], alpha=1, lw=1.5)
+            axs[i+1].axvline(x=s_m, c='k', linestyle=':', lw=0.25)
+            axs[i+1].axvline(x=e_m, c='k', linestyle=':', lw=0.25)
 
     plt.tight_layout()
     return fig, axs
-
-def plot_ground_truth(series, gt):
-    if series.ndim == 1:
-        series = np.expand_dims(series, axis=1)
-    n = len(series)
-    fig, ax = plt.subplots(figsize=(12, 2), sharex=True, sharey=True)
-    ax.set_prop_cycle(cycler(color=[u'#00407a', u'#2ca02c', u'#c00000']))
-    ax.plot(range(len(series)), series, lw=1)
-    ax.legend([f"dim {d}" for d in range(series.shape[1])])
-    ax.set_xlim((0, n))
-    plot_ground_truth_ax(ax, gt, n)
-    return fig, ax
-    
 
 def plot_ground_truth_ax(ax, gt, n):
     for key in gt.keys():
         for (s, e) in gt[key]:
             ax.axvline(x=s, c='k', linestyle=':', lw=0.25)
             ax.axvline(x=e, c='k', linestyle=':', lw=0.25)
-
+            
             text_x = (s + ((e - s) // 2)) / float(n)
             text_y = 0.90
-            ax.text(text_x, text_y, str(key), horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+            ax.text(text_x, text_y, str(key), horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize=MEDIUM_SIZE)
     plt.tight_layout()
     return ax
 
